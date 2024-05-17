738b5a98a62310ab9451ac3dbf47b8b440f7191b
Benzazoua <billalben252@gmail.com>
2024-05-17 11:03:21 +0100

    NAVBAR TOGGLE FOR MOBILE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
 "use strict";
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  elements.forEach((element) => element.addEventListener(eventType, callback));
+};
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
+ * HEADER
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) header.classList.add("active");
+  else header.classList.remove("active");
+});
+
+/**
+ * toggle navbar & overlay when click any navbar-link
+ */
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+addEventOnElements(navLinks, "click", toggleNavbar);
